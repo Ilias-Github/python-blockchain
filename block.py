45d60f0d6023b951cbc608618bf22cde4a79afd8
Ilias Azagagh <toddies_realms_0r@icloud.com>
2024-11-29 09:16:55 +0100

    Format block representation in output

--- block.py
+++ block.py
@@ -9,3 +9,8 @@ class Block:
         self.timestamp = time
         self.transactions = transactions
         self.proof = proof
+
+    # Returns a printable representation of the given object
+    def __repr__(self):
+        return 'Index: {}, Previous Hash: {}, Proof: {}, Transactions: {}'.format(self.index, self.previous_hash,
+                                                                                  self.proof, self.transactions)
